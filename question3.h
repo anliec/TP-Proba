b51096a60e816eb63ba2934df8d2eb15ddcdb065
Nicolas <nicolas.six@pau.fr>
2016-02-08 17:05:08 +0100

    implementation of question 3 file created for question 4

---
+++ question3.h
@@ -0,0 +1,9 @@
+#ifndef QUESTION3_H
+#define QUESTION3_H
+
+double Runs(unsigned *tab, int tabLenght, int valLenght);
+
+double oneProportion(unsigned *tab, int tabLenght, int valLenght);
+int numberOfContinousSuite(unsigned *tab, int tabLenght, int valLenght);
+
+#endif
